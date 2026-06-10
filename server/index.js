c04503f29958668c9585eaee7796a9daaa9ed888
mohamedabbouda <mohamedabbouda54@gmail.com>
2026-06-10 02:09:54 +0200

    Clean Socket.IO server configuration

--- server/index.js
+++ server/index.js
@@ -27,8 +27,8 @@ app.use("/api/auth", AuthRoutes);
 app.use("/api/messages", MessageRoutes);
 
 const server = app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-  console.log(`client url allowed by cors: ${CLIENT_URL}`);
+  console.log(`Server started on port ${PORT}`);
+  console.log(`Allowed client origin: ${CLIENT_URL}`);
 });
 
 const io = new Server(server, {
@@ -41,16 +41,10 @@ const io = new Server(server, {
 global.onlineUsers = new Map();
 
 io.on("connection", (socket) => {
-  console.log("Socket connected:", socket.id);
-
   global.chatSocket = socket;
 
   socket.on("add-user", (userId) => {
-    console.log("add-user received:", userId, "socket:", socket.id);
-
     onlineUsers.set(userId, socket.id);
-
-    console.log("Current online users:", Array.from(onlineUsers.entries()));
 
     socket.broadcast.emit("online-users", {
       onlineUsers: Array.from(onlineUsers.keys()),
@@ -58,11 +52,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("signout", (id) => {
-    console.log("signout received:", id);
-
     onlineUsers.delete(id);
-
-    console.log("Current online users after signout:", Array.from(onlineUsers.entries()));
 
     socket.broadcast.emit("online-users", {
       onlineUsers: Array.from(onlineUsers.keys()),
@@ -80,7 +70,10 @@ io.on("connection", (socket) => {
       });
     } else {
       const senderSocket = onlineUsers.get(data.from);
-      socket.to(senderSocket).emit("voice-call-offline");
+
+      if (senderSocket) {
+        socket.to(senderSocket).emit("voice-call-offline");
+      }
     }
   });
 
@@ -103,7 +96,10 @@ io.on("connection", (socket) => {
       });
     } else {
       const senderSocket = onlineUsers.get(data.from);
-      socket.to(senderSocket).emit("video-call-offline");
+
+      if (senderSocket) {
+        socket.to(senderSocket).emit("video-call-offline");
+      }
     }
   });
 
@@ -124,44 +120,33 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send-msg", (data) => {
-    console.log("send-msg received:", data);
-    console.log("Looking for receiver:", data.to);
-    console.log("Online users map:", Array.from(onlineUsers.entries()));
-
     const sendUserSocket = onlineUsers.get(data.to);
 
     if (sendUserSocket) {
-      console.log("Sending msg-recieve to socket:", sendUserSocket);
-
       socket
         .to(sendUserSocket)
         .emit("msg-recieve", { from: data.from, message: data.message });
-    } else {
-      console.log("Receiver socket not found for user:", data.to);
     }
   });
 
   socket.on("mark-read", ({ id, recieverId }) => {
-    console.log("mark-read received:", { id, recieverId });
-
     const sendUserSocket = onlineUsers.get(id);
 
     if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("mark-read-recieve", { id, recieverId });
+      socket.to(sendUserSocket).emit("mark-read-recieve", {
+        id,
+        recieverId,
+      });
     }
   });
 
   socket.on("disconnect", () => {
-    console.log("Socket disconnected:", socket.id);
-
     for (const [userId, socketId] of onlineUsers.entries()) {
       if (socketId === socket.id) {
         onlineUsers.delete(userId);
         break;
       }
     }
-
-    console.log("Current online users after disconnect:", Array.from(onlineUsers.entries()));
 
     socket.broadcast.emit("online-users", {
       onlineUsers: Array.from(onlineUsers.keys()),
